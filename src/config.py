b1cb83a7c55a61c558b2f98e896f9f619b086d44
kasuba-badri-vishal <kasubabadri@gmail.com>
2022-10-26 21:36:06 +0530

    Added Single Player Game with basic logic

---
+++ src/config.py
@@ -0,0 +1,11 @@
+import os
+
+IMAGES = "./../data/images/"
+AUDIO  = "./../data/audio/"
+FONTS  = "./../data/fonts/"
+
+BACKGROUND_IMAGES = os.path.join(IMAGES,"backgrounds")
+ASTRONOMER_IMAGES = os.path.join(IMAGES,"astronomers")
+PLAYER_IMAGES = os.path.join(IMAGES,"players")
+BULLET_IMAGES = os.path.join(IMAGES,"bullets")
+ENEMY_IMAGES = os.path.join(IMAGES,"enemy")
