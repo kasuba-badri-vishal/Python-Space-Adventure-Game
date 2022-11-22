8ee80f6c6becf0a93a90c28777d95373f46a2604
kasuba-badri-vishal <kasubabadri@gmail.com>
2022-11-23 01:51:00 +0530

    Modularized the Code and updated the Files

--- src/app.py
+++ src/app.py
@@ -1,41 +1,40 @@
-from config import *
-import logging as lg
+from config import HOST, PORT
+from utils import get_stats
 from flask import Flask, render_template
 
-#logging basic configuration
-# lg.basicConfig(filename="log.txt",level=lg.DEBUG)
 
+### Initializing Flask Application with the required information and details of the location of html files and static data file paths
 app = Flask(__name__, static_url_path='', static_folder='web/static', template_folder='web/templates')
 app.config["DEBUG"] = True
 
-
-@app.route('/',methods=['GET', 'POST'])
+### Functions of GET requests to fetch the informat of different HTML pages
+@app.route('/',methods=['GET'])
 def index():
     return render_template('home.html')
 
-@app.route('/about',methods=['GET', 'POST'])
+@app.route('/about',methods=['GET'])
 def about_game():
     return render_template('about.html')
            
-@app.route('/rules',methods=['GET', 'POST'])
+@app.route('/rules',methods=['GET'])
 def rules():
     return render_template('rules.html')
 
-@app.route('/stats',methods=['GET', 'POST'])
+@app.route('/stats',methods=['GET'])
 def game_stats():
     return render_template('stats.html')
            
-@app.route('/experience',methods=['GET', 'POST'])
+@app.route('/experience',methods=['GET'])
 def feedback():
     return render_template('feedback.html')
 
 
 
 
-
-
-
 if __name__ == '__main__':
-	# when first starting the app from docker, we load the model into memory
+    # Before UI is shown, the stats of the Game are updated
+    get_stats()
+
+    # when first starting the app from docker, we load the model into memory
 	# and then start the flask app
-	app.run(debug=False, host=HOST, port=PORT, use_reloader=False)
+    app.run(debug=False, host=HOST, port=PORT, use_reloader=False)
